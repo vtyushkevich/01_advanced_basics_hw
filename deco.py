fcb7b7711b5812495151b2d5eb0787e5d8f0c193
vadim <vadim.tyushkevich@yandex.ru>
2022-12-08 00:04:24 +0500

    Edit deco.py @countcalls @n_ary

--- deco.py
+++ deco.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import functools
+from functools import wraps
 
-from functools import update_wrapper, wraps, lru_cache
 
-
-def disable():
+def disable(func):
     """
     Disable a decorator by re-assigning the decorator's name
     to this function. For example, to turn off memoization:
@@ -12,29 +12,31 @@ def disable():
     # >>> memo = disable
 
     """
-    def wrapper():
-        pass
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
     return wrapper
 
 
-def decorator(f):
+def decorator(func):
     """
     Decorate a decorator so that it inherits the docstrings
     and stuff from the function it's decorating.
     """
-    @wraps(f)
+    @wraps(func)
     def wrapper(*args, **kwargs):
-        return f(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper
 
 
 def countcalls(func):
     """Decorator that counts calls made to the function decorated."""
-    def helper(*args, **kwargs):
-        helper.calls += 1
-        return func(*args, **kwargs)
-    helper.calls = 0
-    return helper
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        result = func(*args, **kwargs)
+        return result
+    wrapper.calls = 0
+    return wrapper
 
 
 def memo(func):
@@ -44,24 +46,33 @@ def memo(func):
     """
     cache = {}
 
-    def wrapper(*args):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         if args in cache:
             return cache[args]
         else:
-            cache[args] = func(*args)
+            cache[args] = func(*args, **kwargs)
             return cache[args]
     return wrapper
 
 
-def n_ary():
+def n_ary(func):
     """
     Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x.
     """
-    return
+    def wrapper(*args):
+        result = None
+        for n in reversed(args):
+            if result is None:
+                result = n
+            else:
+                result = func(n, result)
+        return result
+    return wrapper
 
 
-def trace():
+def trace(str_trace):
     """Trace calls made to function decorated.
 
     @trace("____")
@@ -81,11 +92,17 @@ def trace():
      <-- fib(3) == 3
 
     """
-    return
+    def trace_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(func.__name__, args, kwargs, str_trace)
+            return func(*args, **kwargs)
+        return wrapper
+    return trace_decorator
 
 
-@memo
 @countcalls
+@memo
 @n_ary
 def foo(a, b):
     return a + b
@@ -107,19 +124,20 @@ def fib(n):
 
 
 def main():
+    print(foo(4))
     print(foo(4, 3))
     print(foo(4, 3, 2))
-    print(foo(4, 3))
+    # print(foo(4, 3))
     print("foo was called", foo.calls, "times")
-
-    print(bar(4, 3))
-    print(bar(4, 3, 2))
-    print(bar(4, 3, 2, 1))
-    print("bar was called", bar.calls, "times")
-
-    print(fib.__doc__)
-    fib(3)
-    print(fib.calls, 'calls made')
+    #
+    # print(bar(4, 3))
+    # print(bar(4, 3, 2))
+    # print(bar(4, 3, 2, 1))
+    # print("bar was called", bar.calls, "times")
+    #
+    # print(fib.__doc__)
+    # fib(3)
+    # print(fib.calls, 'calls made')
 
 
 if __name__ == '__main__':
